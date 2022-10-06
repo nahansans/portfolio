25b7cd20ed3aae04a8bf9ce2c5708acd0e69a01a
Naufal <nahanifg@gmail.com>
2022-10-06 21:23:48 +0700

    update add skills, education, bio

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,14 @@ import Top from "@/components/Top"
 import Image from "next/image"
 import ArrowBottom from "@/public/icons/arrow-bottom.png"
 import ImageMe from "@/public/images/me.png"
+import LaravelImage from "@/public/images/laravel.png"
+import ReactImage from "@/public/images/react-js.png"
+import VueImage from "@/public/images/vue-js.png"
+import NextImage from "@/public/images/next-js.png"
+import JSImage from "@/public/images/js.png"
+import PhpImage from "@/public/images/php.png"
+import PsqlImage from "@/public/images/psql.png"
+import MysqlImage from "@/public/images/mysql.png"
 
 
 export default function Home() {
@@ -18,14 +26,14 @@ export default function Home() {
       <Header executeScroll={executeScroll} />
       <div className="container py-[100px] bg-round">
         <div>
-          <h1 className="uppercase fs-monument text-[40px] leading-[35px] md:text-[160px] md:leading-[130px]">
+          <h1 className="title-custom">
             WEB DEVELOPER & TECH ENTHUSIAST
           </h1>
         </div>
         <div className="flex justify-between items-end">
           <img
             onClick={executeScroll}
-            src={ArrowBottom.src} alt="" className="w-[48px] md:w-[118px] mt-[22px] md:mt-[22px] cursor-pointer" />
+            src={ArrowBottom.src} alt="" className="w-[48px] md:w-[80px] 2xl:w-[118px] mt-[22px] md:mt-[22px] cursor-pointer" />
           <a href="" className="text-normal">
             About Hanif
           </a>
@@ -37,12 +45,14 @@ export default function Home() {
 
         <h2 
           ref={contentRef}
-          className="uppercase fs-monument text-[40px] leading-[35px] md:text-[160px] md:leading-[130px] mt-[80px] md:mt-[170px] text-center mb-[30px] md:mb-[60px]">HANIF</h2>
+          className="title-custom pt-[80px] md:pt-[170px] text-center mb-[30px] md:mb-[60px]">
+            NAUFAL<br />HANIF
+          </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
               <p className="text-[14px] md:text-[16px] text-justify">
-                After 10+ years of experience in the digital field as former Lead Art Director at Uplabico, Hyper Island Alumni. I decided to dedicate myself to the wonderful world of freelancing.
+              Hi, I'm Hanif, Naufal Hanif. I am an IT Professional who has experience in technology and web application development. I am also very interested in learning new things related to the development of the IT world. I'm also good at @Laravel @JavaScript @Nextjs @VueJs @PHP @ ReactNative
               </p>
               <div className="">
                 <img src={ImageMe.src} alt="" className="w-[273px] mx-auto" />
@@ -72,56 +82,109 @@ export default function Home() {
             </div>
             <div className="pb-[10px] border-b border-black mt-[40px] md:mt-[60px]">
               <span className="text-[20px] md:text-[40px]">
-                Curriculum
+                Education
               </span>
             </div>
             <div className="mt-[20px]">
               <div className="flex justify-between items-center">
                 <p className="text-[14px] md:text-[16px]">
-                  2xxx - 2xxx
+                  2022 - Present
                 </p>
                 <p className="text-[14px] md:text-[16px]">
-                  Unknown
+                  S1 Information Systems
                 </p>
                 <p className="text-[14px] md:text-[16px]">
-                  Position ??
-                </p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-[14px] md:text-[16px]">
-                  2xxx - 2xxx
-                </p>
-                <p className="text-[14px] md:text-[16px]">
-                  Unknown
-                </p>
-                <p className="text-[14px] md:text-[16px]">
-                  Position ??
-                </p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-[14px] md:text-[16px]">
-                  2xxx - 2xxx
-                </p>
-                <p className="text-[14px] md:text-[16px]">
-                  Unknown
-                </p>
-                <p className="text-[14px] md:text-[16px]">
-                  Position ??
-                </p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-[14px] md:text-[16px]">
-                  2xxx - 2xxx
-                </p>
-                <p className="text-[14px] md:text-[16px]">
-                  Unknown
-                </p>
-                <p className="text-[14px] md:text-[16px]">
-                  Position ??
+                  Universitas Terbuka
                 </p>
               </div>
             </div>
           </div>
+        </div>
+
+        <div className="mt-[40px] md:mt-[60px] grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="order-last md:order-first grid grid-cols-2 md:grid-cols-4 gap-4">
+            <div className="text-center">
+              <a href="" target="_blank">
+                <img src={LaravelImage.src} alt="" className="image-skills mx-auto" />
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="" target="_blank">
+                <img src={VueImage.src} alt="" className="image-skills mx-auto" />
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="" target="_blank">
+                <img src={ReactImage.src} alt="" className="image-skills mx-auto" />
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="" target="_blank">
+                <img src={NextImage.src} alt="" className="image-skills mx-auto" />
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="" target="_blank">
+                <img src={JSImage.src} alt="" className="image-skills mx-auto" />
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="" target="_blank">
+                <img src={PhpImage.src} alt="" className="image-skills mx-auto" />
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="" target="_blank">
+                <img src={PsqlImage.src} alt="" className="image-skills mx-auto" />
+              </a>
+            </div>
+            <div className="text-center">
+              <a href="" target="_blank">
+                <img src={MysqlImage.src} alt="" className="image-skills mx-auto" />
+              </a>
+            </div>
+          </div>
+          <div>
+            <div className="pb-[10px] border-b border-black">
+                <span className="text-[20px] md:text-[40px]">
+                  Skills
+                </span>
+            </div>
+            <div className="mt-[20px]">
+              <div className="flex justify-between mb-2">
+                <p className="text-[14px] md:text-[16px]">
+                  PHP
+                </p>
+                <p className="text-[14px] md:text-[16px] text-right">
+                  Laravel
+                </p>
+              </div>
+              <div className="flex justify-between mb-2">
+                <p className="text-[14px] md:text-[16px]">
+                  Javascript
+                </p>
+                <p className="text-[14px] md:text-[16px] text-right">
+                  Vue JS
+                  <br />
+                  Next JS
+                  <br />
+                  React JS
+                  <br />
+                  React Native
+                </p>
+              </div>
+              <div className="flex justify-between mb-2">
+                <p className="text-[14px] md:text-[16px]">
+                  SQL
+                </p>
+                <p className="text-[14px] md:text-[16px] text-right">
+                  MySQL
+                  <br />
+                  PostgreSQL
+                </p>
+              </div>
+            </div>
+            </div>
         </div>
       </div>
     </>
