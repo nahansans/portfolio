c74854266dd3f7f6888d5989872e42b60201fd15
Naufal <nahanifg@gmail.com>
2023-02-05 13:17:14 +0700

    adding contact us

--- components/Top.js
+++ components/Top.js
@@ -3,7 +3,16 @@ import Head from "next/head";
 export default function Top() {
     return (
         <Head>
+            <title>Portfolio - Web Developer</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="title" content="Portfolio" />
+            <meta name="description" content="Website Developer available for WHO, WFO, or Hybrid. Middle Web Engineer, 19 years old" />
+            <meta name="keywords" content="website developer, website developer near me, website developer jakarta, website developer adalah, freelance web developer, freelance web developers, freelance web, freelance web developer indonesia, portfolio, portfolio website, php, php programmer, lowongan php programmer, need php programmer, jasa pembuatan website, website, dicari programmer php, freelance job programmer php, programmer semarang, jasa programmer semarang, freelancer programmer semarang" />
+            <meta name="robots" content="index, follow" />
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="author" content="Naufal Hanif" />
+
         </Head>
     )
 }
